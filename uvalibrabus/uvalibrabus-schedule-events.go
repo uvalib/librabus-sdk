911bc823e58934e4af01f0d18f61f184db41f1cf
Dave Goldstein <dpg3k@virginia.edu>
2024-03-20 09:50:50 -0400

    Add schedule events

---
+++ uvalibrabus/uvalibrabus-schedule-events.go
@@ -0,0 +1,15 @@
+//
+// Events related to scheduling
+//
+
+package uvalibrabus
+
+//
+// event names
+//
+
+var EventScheduleEtdIngest = "schedule.etd.ingest" // time to ingest etd content
+
+//
+// end of file
+//
