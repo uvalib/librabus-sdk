57ba633b6d1b307442651bb2f6bc3e461c54d061
Dave Goldstein <dpg3k@virginia.edu>
2024-03-25 11:28:56 -0400

    Support publish from a file

--- tools/librabus-pub/main.go
+++ tools/librabus-pub/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/uvalib/librabus-sdk/uvalibrabus"
 	"log"
 	"os"
+	"strings"
 )
 
 // main entry point
@@ -18,12 +19,14 @@ func main() {
 	var logger *log.Logger
 	var namespace string
 	var oid string
+	var bulk string
 
 	flag.StringVar(&eventBus, "bus", "", "Event bus name")
 	flag.StringVar(&eventSource, "source", "", "Event source name")
 	flag.StringVar(&eventName, "event", "", "The name of the event")
 	flag.StringVar(&namespace, "ns", "", "The event namespace")
 	flag.StringVar(&oid, "oid", "", "The event object identifier")
+	flag.StringVar(&bulk, "bulk", "", "File of events to publish (name/namespace/oid)")
 	flag.BoolVar(&debug, "debug", false, "Log debug information")
 	flag.Parse()
 
@@ -31,16 +34,22 @@ func main() {
 		logger = log.Default()
 	}
 
-	// validate parameters
+	// validate required parameters
 	if len(eventBus) == 0 ||
-		len(eventSource) == 0 ||
-		len(eventName) == 0 ||
-		len(namespace) == 0 ||
-		len(oid) == 0 {
+		len(eventSource) == 0 {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
+	// validate optional parameters
+	if len(bulk) == 0 && (len(eventName) == 0 ||
+		len(namespace) == 0 ||
+		len(oid) == 0) {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// create the bus client
 	cfg := uvalibrabus.UvaBusConfig{
 		Source:  eventSource,
 		BusName: eventBus,
@@ -52,19 +61,52 @@ func main() {
 	}
 	fmt.Printf("Using: %s@%s\n", eventSource, eventBus)
 
-	// for now, just 1 event type
+	if len(bulk) != 0 {
+
+		var bytesRead []byte
+		bytesRead, err = os.ReadFile(bulk)
+		if err == nil {
+			lines := strings.Split(string(bytesRead), "\n")
+			for _, line := range lines {
+				attrs := strings.Split(line, "/")
+				if len(attrs) == 3 {
+					err = publishEvent(bus, attrs[0], attrs[1], attrs[2])
+					if err != nil {
+						break
+					}
+				} else {
+					if len(line) != 0 {
+						fmt.Printf("WARNING: ignoring: %s\n", line)
+					}
+				}
+			}
+		}
+	} else {
+		err = publishEvent(bus, eventName, namespace, oid)
+	}
+
+	if err != nil {
+		fmt.Printf("Terminating with error (%s)\n", err.Error())
+		os.Exit(1)
+	} else {
+		fmt.Printf("Terminating normally\n")
+	}
+}
+
+func publishEvent(bus uvalibrabus.UvaBus, name string, namespace string, oid string) error {
+
 	ev := uvalibrabus.UvaBusEvent{
-		EventName:  eventName,
+		EventName:  name,
 		Namespace:  namespace,
 		Identifier: oid,
 	}
-	err = bus.PublishEvent(ev)
+	err := bus.PublishEvent(ev)
 	if err != nil {
-		log.Fatalf("ERROR: publishing event (%s)", err.Error())
+		return fmt.Errorf("publishing event (%s)", err.Error())
 	}
 
-	fmt.Printf("Published: %s\n", ev.String())
-	fmt.Printf("Terminating normally\n")
+	fmt.Printf("INFO: published: %s\n", ev.String())
+	return nil
 }
 
 //
