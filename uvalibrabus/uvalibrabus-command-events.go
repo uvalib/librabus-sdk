00afbdea761cf91bb8828c01de0226adff3276a6
Dave Goldstein <dpg3k@virginia.edu>
2024-03-25 10:56:37 -0400

    Add some command events... we are going to need these

---
+++ uvalibrabus/uvalibrabus-command-events.go
@@ -0,0 +1,19 @@
+//
+// Events related to scheduling
+//
+
+package uvalibrabus
+
+//
+// event names
+//
+
+var EventCommandReindex = "command.work.reindex"       // command to reindex for search
+var EventCommandDoiSync = "command.work.doisync"       // command to DOI resync
+var EventCommandOrcidSync = "command.work.orcidsync"   // command to ORCID resync
+var EventCommandMailInvite = "command.mail.invitation" // command to mail invitation
+var EventCommandMailSuccess = "command.mail.success"   // command to mail success
+
+//
+// end of file
+//
