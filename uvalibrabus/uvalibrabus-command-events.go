3ad91346ed46cff7b0bc7f2e2388806f6e1312e6
Dave Goldstein <dpg3k@virginia.edu>
2024-04-16 14:20:04 -0400

    Add command.sis.notify

--- uvalibrabus/uvalibrabus-command-events.go
+++ uvalibrabus/uvalibrabus-command-events.go
@@ -13,6 +13,7 @@ var EventCommandDoiSync = "command.work.doisync"       // command to DOI resync
 var EventCommandOrcidSync = "command.work.orcidsync"   // command to ORCID resync
 var EventCommandMailInvite = "command.mail.invitation" // command to mail invitation
 var EventCommandMailSuccess = "command.mail.success"   // command to mail success
+var EventCommandSisNotify = "command.sis.notify"       // command to notify SIS
 
 //
 // end of file
