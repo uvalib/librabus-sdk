0b78bc8b863fe6883d2a5c655e4e442c06884c47
Dave G <dpg3k@virginia.edu>
2025-05-20 10:09:39 -0400

    Additional command event for resubmission to virgo

--- uvalibrabus/uvalibrabus-command-events.go
+++ uvalibrabus/uvalibrabus-command-events.go
@@ -9,6 +9,7 @@ package uvalibrabus
 //
 
 var EventCommandReindex = "command.work.reindex"       // command to reindex for search
+var EventCommandRevirgo = "command.work.revirgo"       // command to reexport for virgo
 var EventCommandDoiSync = "command.work.doisync"       // command to DOI resync
 var EventCommandOrcidSync = "command.work.orcidsync"   // command to ORCID resync
 var EventCommandMailInvite = "command.mail.invitation" // command to mail invitation
